cc75a269b5725fd0640950ab381c09560ba26e14
HanyuuFurute <1814778829@qq.com>
2018-11-22 19:39:02 +0800

    404定时跳转功能实现

--- webpage/js/treemenu.js
+++ webpage/js/treemenu.js
@@ -4,9 +4,11 @@ function show(d1){
 			document.getElementById('p2').style.display='block';
 			document.getElementById('p1').style.display='none';
 	 	}else{
-			
 			document.getElementById(d1).style.display='none';  //触动的层如果处于显示状态，即隐藏
 	 		document.getElementById('p2').style.display='none';
 			document.getElementById('p1').style.display='block';
 	   	}
 	}
+function jump(){
+	setTimeout("javascript:location.href='index.html'", 100); 
+}
