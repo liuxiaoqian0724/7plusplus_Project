4550b05e79beb5b86d6bf25214982371ca6fd648
cuishan2000 <760543074@qq.com>
2018-11-26 20:15:10 +0800

    update

--- webpage/js/tutorlisted.js
+++ webpage/js/tutorlisted.js
@@ -8,21 +8,54 @@
 // })
 
 $(document).ready(function(){
+	var a = new Array();
+	$('#spe3').click(function(){
+			// alert("123");
+		$('.title-condition').remove();
 
-				$('#spe3').click(function(){
-					// alert("123");
-					$('.title-condition').remove();
+	})
+	var search = document.getElementById("partInput");
+	search.onfocus = function(){
+	    search.value="";
+	}
 
-				})
-				// $('.con-li').click(function(event){
-				// 	var $content=this.innerHTML;
-				// 	// alert($content);
-				// 	var $condition=$("<li class='title-condition'> &nbsp;<a href='' class='aa'></a><i class='icon1 ion-close'></i></li>");
-				// 	$('.title-ul').append($condition);
-				// 	$('.aa').html($content);
-				// })
-				var search = document.getElementById("partInput");
-				  search.onfocus = function(){
-				      search.value="";
-				  }
+	
+
+	//添加子元素并保存
+	$('.con-li').click(function(enent){
+		var content=$(this).html();
+		var $element=$("<li class='title-condition'>"+content+"&nbsp;"+'<i class="icon1 ion-close"></i></li>');
+		alert(content);
+		alert($('.icon').attr('class'));
+		$(".title-ul").prepend($element);
+		// a.push(content);
+		// // $.session.set('conditions', a);
+		// sessionStorage.setItem('conditions', a);
+		// alert(sessionStorage.getItem("conditions")[1]);
+	})
+	$(".icon1").click(function(event){
+		alert(123);
+		alert($(this).parent().attr('class'));
+		$(this).parent().remove();
+	})
+	var i=0;
+	$('#more').click(function(){
+		if(i%2==1){
+			$('#more').html('更多'+'<i class="icon11 icon-angle-down"></i>'); 
+		}else{
+			$('#more').html('收起'+'<i class="icon11 icon-angle-up"></i>')
+		}
+		i++;
+		$('.hide-con-content').toggle();
+	})
+	var j=0;
+	$('#more1').click(function(){
+		if(i%2==1){
+			$('#more1').html('更多'+'<i class="icon11 icon-angle-down"></i>'); 
+		}else{
+			$('#more1').html('收起'+'<i class="icon11 icon-angle-up"></i>');
+		}
+		i++;
+		$('.hide-con-content1').toggle();
+	})
 })
