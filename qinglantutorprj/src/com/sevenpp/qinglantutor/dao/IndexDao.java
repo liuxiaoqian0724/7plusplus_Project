c196f0a828827ccd8445e9a8a3dfc4ed4520aa80
1406628376@qq.com <renjiahua12345>
2018-12-17 17:34:47 +0800

    up

---
+++ qinglantutorprj/src/com/sevenpp/qinglantutor/dao/IndexDao.java
@@ -0,0 +1,41 @@
+/**   
+		* @Title: IndexDao.java 
+		* @Package com.sevenpp.qinglantutor.dao 
+		* @Description: TODO(用一句话描述该文件做什么) 
+		* @author （作者）  
+		* @date 2018年12月7日 下午7:27:33 
+		* @version V1.0   
+		*/
+	
+package com.sevenpp.qinglantutor.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sevenpp.qinglantutor.entity.User;
+
+/**
+		*
+		* 项目名称：qinglantutorprj
+		* 类名称：IndexDao
+		* 类描述：
+		* 创建人：rain
+		* 创建时间：2018年12月7日 下午7:27:33
+		* 修改人：rain
+		* 修改时间：2018年12月7日 下午7:27:33
+		* 修改备注：
+		* @version
+		*
+		*/
+		/** 
+		* @ClassName: IndexDao 
+		* @Description: TODO(这里用一句话描述这个类的作用) 
+		* @author (作者)  
+		* @date 2018年12月7日 下午7:27:33 
+		* @version V1.0 
+		*/
+@Component
+public interface IndexDao {
+	public List<User> queryAll();
+}
---
+++ qinglantutorprj/src/com/sevenpp/qinglantutor/service/IndexService.java
@@ -0,0 +1,34 @@
+
+
+package com.sevenpp.qinglantutor.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sevenpp.qinglantutor.entity.Userinf;
+
+/**
+		*
+		* 项目名称：qinglantutorprj
+		* 类名称：IndexService
+		* 类描述：
+		* 创建人：rain
+		* 创建时间：2018年12月7日 下午8:23:27
+		* 修改人：rain
+		* 修改时间：2018年12月7日 下午8:23:27
+		* 修改备注：
+		* @version
+		*
+		*/
+		/** 
+		* @ClassName: IndexService 
+		* @Description: TODO(这里用一句话描述这个类的作用) 
+		* @author (作者)  
+		* @date 2018年12月7日 下午8:23:27 
+		* @version V1.0 
+		*/
+@Component
+public interface IndexService {
+	public List<Userinf> findAllUser();
+}
