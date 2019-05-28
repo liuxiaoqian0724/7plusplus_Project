8d8cf63d072a52679e7d2a41d4211ddc7c25725d
cuishan2000 <760543074@qq.com>
2019-05-28 15:02:04 +0800

    update

--- qinglantutorprj/src/com/sevenpp/qinglantutor/controller/tutorlist/TutorAsCandidateController.java
+++ qinglantutorprj/src/com/sevenpp/qinglantutor/controller/tutorlist/TutorAsCandidateController.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.sevenpp.qinglantutor.service.impl.TutorDetailServiceImpl;
 import com.sevenpp.qinglantutor.utils.cookie.CookieUtils;
 
