3079f92e34acf63a6a7c8ddf32c27083fc4ff071
cuishan2000 <760543074@qq.com>
2019-05-28 15:20:19 +0800

    update

--- qinglantutorprj/src/com/sevenpp/qinglantutor/controller/tutorlist/TutorAsCandidateController.java
+++ qinglantutorprj/src/com/sevenpp/qinglantutor/controller/tutorlist/TutorAsCandidateController.java
@@ -11,6 +11,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.SpiderWebPlot;
@@ -20,15 +21,12 @@ import org.jfree.chart.title.TextTitle;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.RectangleEdge;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.sevenpp.qinglantutor.entity.User;
 import com.sevenpp.qinglantutor.entity.Userinfosec;
-import com.sevenpp.qinglantutor.service.impl.TutorAsCandidateServiceImpl;
 import com.sevenpp.qinglantutor.service.impl.TutorDetailServiceImpl;
 import com.sevenpp.qinglantutor.utils.cookie.CookieUtils;
 
@@ -36,11 +34,9 @@ import com.sevenpp.qinglantutor.utils.cookie.CookieUtils;
 public class TutorAsCandidateController {
 	@Resource
 	private TutorDetailServiceImpl tutorDetailServiceImpl;
-	private TutorAsCandidateServiceImpl tutorAsCandidateServiceImpl;
 	
 	@RequestMapping(value="/findCandiateInfo",method=RequestMethod.GET)
-	public String sendMsgToTutor(@RequestParam("arr") String arr,HttpServletRequest request,HttpServletResponse response,
-								Model model,ModelAndView mv) throws IOException {
+	public void sendMsgToTutor(@RequestParam("arr") String arr,HttpServletRequest request,HttpServletResponse response,HttpSession session) throws IOException {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST");
 		response.setHeader("Access-Control-Allow-Headers","x-requested-with,content-type");
@@ -57,31 +53,19 @@ public class TutorAsCandidateController {
 		Matcher m = p.matcher(arr);
 		String result = m.replaceAll("");
 		for (int i = 0; i < result.length(); i++) {
-			digitList.add(result.substring(i, i+1));	 
+		digitList.add(result.substring(i, i+1));	 
 		}
-		System.out.println(digitList);//候选老师的id集合
 		response.setCharacterEncoding("utf-8");
-		System.out.println(digitList);
-		//得到User集合
 		List<User> users=new ArrayList<User>();
-		System.out.println(users);
 		if(!digitList.isEmpty()) {
-			System.out.println("not empty");
-//			this.tutorAsCandidateServiceImpl.findUsers(digitList);
 			int id=0;
 			for (String digit : digitList) {
 				id=Integer.parseInt(digit);
-				System.out.println("id:"+id);
 				if(this.tutorDetailServiceImpl.getTutorDetail(id)!=null)
 					users.add(this.tutorDetailServiceImpl.getTutorDetail(id));
 			}
-			System.out.println("users.size:"+users.size());
-//			if(this.tutorAsCandidateServiceImpl.findUsers(digitList)!=null) {
-//				users=this.tutorAsCandidateServiceImpl.findUsers(digitList);
-//			}
 			//得到Userinfos集合  
 			List<Userinfosec> userinfos=new ArrayList<Userinfosec>();
-//			userinfos=this.tutorAsCandidateServiceImpl.findUserinfosec(users);
 			for (User user : users) {
 				Userinfosec userinfo=new Userinfosec();
 				userinfo.setId(user.getId());
@@ -98,31 +82,26 @@ public class TutorAsCandidateController {
 			String group;
 			for (Userinfosec userinfosec : userinfos) {
 				group=userinfosec.getName();
-				dataset.addValue(userinfosec.getPrice(), group, "价格");
+				dataset.addValue(userinfosec.getPrice()/50, group, "价格");
 				dataset.addValue(userinfosec.getReviewstar(), group, "好评");
 				dataset.addValue(userinfosec.getTeachage(), group, "教龄");
 				dataset.addValue(userinfosec.getTeachsum(), group, "教过的学生数");
-				dataset.addValue(userinfosec.getDataintegrity(), group, "资料完整度");
+				dataset.addValue(userinfosec.getDataintegrity()*5, group, "资料完整度");
 			}
-			return "image";
-//			SpiderWebPlot spiderwebplot = new SpiderWebPlot(dataset);
-//			JFreeChart jfreechart = new JFreeChart("Test", TextTitle.DEFAULT_FONT, spiderwebplot, false);
-//			LegendTitle legendtitle = new LegendTitle(spiderwebplot);
-//			legendtitle.setPosition(RectangleEdge.TOP);
-//			jfreechart.addSubtitle(legendtitle);
-//			ByteArrayOutputStream out = new ByteArrayOutputStream();
-//			String filename = ServletUtilities.saveChartAsPNG(jfreechart, 400, 500, null);
-//			String graphURL = request.getContextPath() + "/DisplayChart?filename="   + filename;
-//			System.out.println(graphURL);
-//			String image=  "<img src='" 
-//		            + graphURL 
-//		            + "' width=400 height=500 border=0 usemap='#" 
-//		            + filename + "'/>"; 
-//			System.out.println("success");
-//			model.addAttribute("radarimage", image);
-//			mv.setViewName("image");
-////			return mv;
+			SpiderWebPlot spiderwebplot = new SpiderWebPlot(dataset);
+			JFreeChart jfreechart = new JFreeChart("Compare", TextTitle.DEFAULT_FONT, spiderwebplot, false);
+			LegendTitle legendtitle = new LegendTitle(spiderwebplot);
+			legendtitle.setPosition(RectangleEdge.TOP);
+			jfreechart.addSubtitle(legendtitle);
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			String filename = ServletUtilities.saveChartAsPNG(jfreechart, 500, 500, null);
+			String graphURL = request.getContextPath() + "/DisplayChart?filename="   + filename;
+			System.out.println(graphURL);
+			String image=  "<img src='" 
+		            + graphURL 
+		            + "' width=400 height=500 border=0 usemap='#" 
+		            + filename + "'/>"; 
+			session.setAttribute("radarimage",image);
 		}
-		return null;
 	}
 }
