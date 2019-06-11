58ce4264b163333a1621eac66c3f1f07a25efb3d
Wangzhaocici <1067861305@qq.com>
2019-06-11 08:29:23 +0800

    文章列表页跳转列表详情页，增加分页

--- qinglantutorprj/WebContent/js/articlelist.js
+++ qinglantutorprj/WebContent/js/articlelist.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+window.onload=function(){
 	//调用
 	topage4(currentpage4);
 	listshow(currentpage4);
-})
+}
 //页码定义
 var currentpage4 = 1;
 var totalpage4 = 1;
@@ -53,7 +53,6 @@ function listshow(currentpage4) {
 							+ '&nbsp;&nbsp;&nbsp;页</span></li>'
 							+ '<li class="next"><a id="next4">»</a></li>';
 					$("#pager4").append(devicepage4);
-					console.log(currentpage4)
 				}
 				// 当前页为最后一页时
 				if (currentpage4 == totalpage4) {
@@ -66,7 +65,6 @@ function listshow(currentpage4) {
 							+ '&nbsp;&nbsp;&nbsp;页</span></li>'
 							+ '<li class="next disabled"><a id="next4">»</a></li>';
 					$("#pager4").append(devicepage4);
-					console.log(currentpage4)
 				}
 				// 当前页为中间页时
 				if (currentpage4 > 1 && currentpage4 < totalpage4) {
@@ -79,24 +77,36 @@ function listshow(currentpage4) {
 							+ '&nbsp;&nbsp;&nbsp;页</span></li>'
 							+ '<li class="next"><a id="next4">»</a></li>';
 					$("#pager4").append(devicepage4);
-					console.log(currentpage4)
 				}
 				if(index<4){
 					articlelist1='<div class="col-md-4 col-sm-6 col-lg-3">'+
-					'<a class="card" href="###" style="position:relative; width:93%; margin-left:1%;">'+
+					'<a class="card" href="teacherdetail?artid='+res.aId+'" style="position:relative; width:93%; margin-left:1%;">'+
 					'<img src="images/img2.jpg" alt="">'+
 					'<div class="card-heading"><strong>'+res.title+'</strong></div>'+
-					'<div class="pull-right"><i class="icon-thumbs-o-up"></i>'+res.likeCount+'</div>'+
+					'<div class="pull-right" style="position:relative;margin-right:3%;"><i class="icon-thumbs-o-up"></i>'+res.likeCount+'</div>'+
 					'</a>'+
 					'</div>';
 					$("#articlel").append(articlelist1);
 				}
 				if (index >= (currentpage4 - 1) * 5
 						&& index <= currentpage4 * 5 - 1) {
+					function repalceHtml(str){
+						var dd=str.replace(/<\/?.+?>/g,"");
+						var dds=dd.replace(/ /g,"");//dds为得到后的内容
+						return dds;
+					}
+					var content=repalceHtml(res.content);
+					var leng=res.content.length;
+		            if(leng>70){
+		            	content=content.substr(0,70)+'……';
+		            }
+		            else{
+		            	content=content;
+		            }
 					articlelist2='<div class="articlecard">'+
 						'<img src="images/course'+(index+1)+'.jpg" class="img-rounded" alt="圆角图片" style="position:relative; margin:1%; width:15%;">'+
-						'<div class="card-heading" style="position: relative; margin-top:-11%; left:20%; width:80%;"><strong>'+res.title+'</strong></div>'+
-						'<div class="card-content text-muted" style="position: relative; left:20%; width:75%;">'+res.content+
+						'<div class="card-heading" style="position: relative; margin-top:-11%; left:20%; width:80%;"><a href="teacherdetail?artid='+res.aId+'"><strong>'+res.title+'</strong></a></div>'+
+						'<div class="card-content text-muted" style="position: relative; left:20%; width:75%;">'+content+
 						'</div>'+
 						'<div class="author">'+
 						'<img src="images/idphotocircle'+(index+1)+'.png" style="position: relative; width: 4%; left:20%; margin:1%;" />'+
